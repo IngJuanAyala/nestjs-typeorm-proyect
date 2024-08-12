117f032c6433108a093a6a1efe6d614550fe4b7c
Juan Ayala <juanayalasalazar1@gmail.com>
2024-08-12 15:43:28 -0500

    install typeorm, create configuration, create clients entity and set connection to database. :sparkles:

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule } from './clients/clients.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ClientsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATASOURCE_HOST,
+      port: parseInt(process.env.DATASOURCE_PORT),
+      username: process.env.DATASOURCE_USERNAME,
+      password: process.env.DATASOURCE_PASSWORD,
+      database:  process.env.DATASOURCE_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize:true
+    }),
+    ClientsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/clients/clients.entity.ts
@@ -0,0 +1,43 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm' 
+
+@Entity()
+export class Client {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    code: number
+
+    @Column({nullable: true})
+    address: string
+
+    @Column({nullable: true})
+    phone: number
+
+    @Column()
+    firstName: string
+
+    @Column()
+    lastName: string
+
+    @Column({unique: true})
+    email: string
+
+    @Column()
+    dni: string
+
+    @Column({ type: 'datetime' })
+    birthDate: Date
+
+    @Column({ type: 'datetime' })
+    enrollmentDate: Date
+
+    @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date
+
+    @Column({nullable: true})
+    interestTopic: string
+
+    @Column()
+    state: boolean
+}
